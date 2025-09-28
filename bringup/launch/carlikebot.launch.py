86524a77bfd8a75bcbad91b6aa51673bea69c69a
kenyam1979 <ken.yamaji@gmail.com>
2025-09-28 12:39:06 +0900

    20250928

--- bringup/launch/carlikebot.launch.py
+++ bringup/launch/carlikebot.launch.py
@@ -50,7 +50,8 @@ def generate_launch_description():
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
             PathJoinSubstitution(
-                [FindPackageShare("ros2_control_demo_example_11"), "urdf", "carlikebot.urdf.xacro"]
+                [FindPackageShare("robot_controller2_ackm"),
+                 "urdf", "carlikebot.urdf.xacro"]
             ),
         ]
     )
@@ -58,18 +59,18 @@ def generate_launch_description():
 
     robot_controllers = PathJoinSubstitution(
         [
-            FindPackageShare("ros2_control_demo_example_11"),
+            FindPackageShare("robot_controller2_ackm"),
             "config",
             "carlikebot_controllers.yaml",
         ]
     )
-    rviz_config_file = PathJoinSubstitution(
-        [
-            FindPackageShare("ros2_control_demo_description"),
-            "carlikebot/rviz",
-            "carlikebot.rviz",
-        ]
-    )
+    # rviz_config_file = PathJoinSubstitution(
+    #     [
+    #         FindPackageShare("ros2_control_demo_description"),
+    #         "carlikebot/rviz",
+    #         "carlikebot.rviz",
+    #     ]
+    # )
 
     # the steering controller libraries by default publish odometry on a separate topic than /tf
     control_node_remapped = Node(
@@ -99,34 +100,36 @@ def generate_launch_description():
         output="both",
         parameters=[robot_description],
     )
-    rviz_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
-        output="log",
-        arguments=["-d", rviz_config_file],
-        condition=IfCondition(gui),
-    )
+    # rviz_node = Node(
+    #     package="rviz2",
+    #     executable="rviz2",
+    #     name="rviz2",
+    #     output="log",
+    #     arguments=["-d", rviz_config_file],
+    #     condition=IfCondition(gui),
+    # )
 
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+        arguments=["joint_state_broadcaster",
+                   "--controller-manager", "/controller_manager"],
     )
 
     robot_bicycle_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["bicycle_steering_controller", "--controller-manager", "/controller_manager"],
+        arguments=["bicycle_steering_controller",
+                   "--controller-manager", "/controller_manager"],
     )
 
     # Delay rviz start after `joint_state_broadcaster`
-    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=joint_state_broadcaster_spawner,
-            on_exit=[rviz_node],
-        )
-    )
+    # delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+    #     event_handler=OnProcessExit(
+    #         target_action=joint_state_broadcaster_spawner,
+    #         on_exit=[rviz_node],
+    #     )
+    # )
 
     # Delay start of joint_state_broadcaster after `robot_controller`
     # TODO(anyone): This is a workaround for flaky tests. Remove when fixed.
@@ -143,7 +146,7 @@ def generate_launch_description():
         robot_state_pub_bicycle_node,
         robot_bicycle_controller_spawner,
         delay_joint_state_broadcaster_after_robot_controller_spawner,
-        delay_rviz_after_joint_state_broadcaster_spawner,
+        # delay_rviz_after_joint_state_broadcaster_spawner,
     ]
 
     return LaunchDescription(declared_arguments + nodes)
